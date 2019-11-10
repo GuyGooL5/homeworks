7ccc5622ab94f5307ad54c55cb964457dcce600c
Guy Tsitsiashvili <guygool4@gmail.com>
2019-11-10 18:54:24 +0200

    changed unnecessary 4.0 to 4 since it's double

--- assignment_2/home/home_2.c
+++ assignment_2/home/home_2.c
@@ -10,7 +10,7 @@ int main()
     scanf("%lf", &radius);
     perimeter = 2 * PI * radius;
     area = PI * pow(radius, 2);
-    volume = (4.0 / 3) * PI * pow(radius, 3);
+    volume = (4 / 3) * PI * pow(radius, 3);
     printf("The circle has: \nPerimeter of %lf, \nArea of %lf, \nVolume of %lf ", perimeter, area, volume);
     return 0;
 }
