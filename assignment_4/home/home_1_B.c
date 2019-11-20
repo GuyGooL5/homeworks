9469a6ae97abf636ce21b6d2511db93d57c4f1d0
Guy Tsitsiashvili <guygool4@gmail.com>
2019-11-20 22:41:17 +0200

    fixed <max to <=max in loops

--- assignment_4/home/home_1_B.c
+++ assignment_4/home/home_1_B.c
@@ -2,17 +2,17 @@
 int main()
 {
     int i = 15, j = 3, k = 5, sum = 0, max = 1000, counter = 0;
-    for (i; i < max; i += 15)
+    for (i; i <= max; i += 15)
     {
         counter++;
         sum += i;
     }
-    for (j; j < max; j += (j + 3) % 15 ? 3 : 6)
+    for (j; j <= max; j += (j + 3) % 15 ? 3 : 6)
     {
         counter++;
         sum += j;
     }
-    for (k; k < max; k += (k + 5) % 15 ? 5 : 10)
+    for (k; k <= max; k += (k + 5) % 15 ? 5 : 10)
     {
         counter++;
         sum += k;
