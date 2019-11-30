ce1f5b9b56b7c7d19d7036631ed23422a3830a6c
GuyGooL5 <guygool5@gmail.com>
2019-11-30 19:08:31 +0200

    class 6

---
+++ assignment_6/class/class_1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int hasEvenDigitRecursive(int n);
+
+int main()
+{
+    int n;
+    printf("Enter a number to see if has any even digit: ");
+    scanf("%d", &n);
+    if (hasEvenDigitRecursive(n))
+        printf("%d has even digit", n);
+    else
+        printf("%d has no even digits", n);
+    return 0;
+}
+
+int hasEvenDigitRecursive(int n)
+{
+    if (n % 2 == 0)
+        return 1;
+    if (n <= 10)
+        return 0;
+    return hasEvenDigitRecursive(n / 10);
+}
